20bfd8f85b1ee86be351a7ded1f4d01d39cd61ee
Luke Monroe <monroe.luke36@gmail.com>
2025-09-21 20:22:17 -0400

    Refactor tasks in lab.js for clarity and structure

--- lab.js
+++ lab.js
@@ -1,96 +1,96 @@
 //buttons at bottom
 
 //task 1
-function task1(){
-const quantity = prompt("Enter a number for the quantity");
-const quantityTag = document.querySelector(".quantity");
+function task1() {
+    const quantity = prompt("Enter a number for the quantity");
+    const quantityTag = document.querySelector(".quantity");
 
 
-quantityTag.innerHTML = `Quantity: ${quantity}`;
+    quantityTag.innerHTML = `Quantity: ${quantity}`;
 
 
-//part b
-const percent = prompt("Enter a percentage: ");
-quantityTag.innerHTML = `${percent}% of the Quantity, ${quantity}, is ${quantity * percent / 100}`;
+    //part b
+    const percent = prompt("Enter a percentage: ");
+    quantityTag.innerHTML = `${percent}% of the Quantity, ${quantity}, is ${quantity * percent / 100}`;
 }
 //task 2
-function task2(){
-const grade = parseInt(prompt("Enter a grade between 1-100: "), 10);
-const gradeTag = document.querySelector(".grade");
-//part a
-let ifComputedGrade;
+function task2() {
+    const grade = parseInt(prompt("Enter a grade between 1-100: "), 10);
+    const gradeTag = document.querySelector(".grade");
+    //part a
+    let ifComputedGrade;
 
 
-if (grade >= 91 && grade <= 100) {
-    ifComputedGrade = "A";
-} else if (grade < 91 && grade >= 81) {
-    ifComputedGrade = "B";
-} else if (grade < 81 && grade >= 71) {
-    ifComputedGrade = "C";
-} else if (grade < 71 && grade >= 61) {
-    ifComputedGrade = "D";
-} else if (grade < 61 && grade >= 0) {
-    ifComputedGrade = "F";
-} else {
-    alert("Enter a number between 0-100");
-}
-
-
-//part b (switch true to check against a range of values)
-let SwitchComputedGrade;
-switch (true) {
-    case grade >= 91 && grade <= 100:
-        SwitchComputedGrade = "A";
-        break;
-    case grade >= 81 && grade < 91:
-        SwitchComputedGrade = "B";
-        break;
-    case grade >= 71 && grade < 81:
-        SwitchComputedGrade = "C";
-        break;
-    case grade >= 61 && grade < 71:
-        SwitchComputedGrade = "D";
-        break;
-    case grade >= 0 && grade < 61:
-        SwitchComputedGrade = "F";
-        break;
-    default:
+    if (grade >= 91 && grade <= 100) {
+        ifComputedGrade = "A";
+    } else if (grade < 91 && grade >= 81) {
+        ifComputedGrade = "B";
+    } else if (grade < 81 && grade >= 71) {
+        ifComputedGrade = "C";
+    } else if (grade < 71 && grade >= 61) {
+        ifComputedGrade = "D";
+    } else if (grade < 61 && grade >= 0) {
+        ifComputedGrade = "F";
+    } else {
         alert("Enter a number between 0-100");
-}
+    }
 
 
-gradeTag.innerHTML = `The Grade is: (a) ${ifComputedGrade}, (b) ${SwitchComputedGrade}`;
+    //part b (switch true to check against a range of values)
+    let SwitchComputedGrade;
+    switch (true) {
+        case grade >= 91 && grade <= 100:
+            SwitchComputedGrade = "A";
+            break;
+        case grade >= 81 && grade < 91:
+            SwitchComputedGrade = "B";
+            break;
+        case grade >= 71 && grade < 81:
+            SwitchComputedGrade = "C";
+            break;
+        case grade >= 61 && grade < 71:
+            SwitchComputedGrade = "D";
+            break;
+        case grade >= 0 && grade < 61:
+            SwitchComputedGrade = "F";
+            break;
+        default:
+            alert("Enter a number between 0-100");
+    }
+
+
+    gradeTag.innerHTML = `The Grade is: (a) ${ifComputedGrade}, (b) ${SwitchComputedGrade}`;
 }
 //task 3
-function task3(){
-const profName = prompt("Enter the name of a punishing professor");
-const lineToWrite = prompt("Enter a line for the user to write");
-const numberToWrite = parseInt(prompt("How many times should the user enter the line?"));
+function task3() {
+    const profName = prompt("Enter the name of a punishing professor");
+    const lineToWrite = prompt("Enter a line for the user to write");
+    const numberToWrite = parseInt(prompt("How many times should the user enter the line?"));
 
-console.log(profName);
-for (let i = 1; i <= numberToWrite; i++){
-    console.log(i + " " + lineToWrite);
-}
+    console.log(profName);
+    for (let i = 1; i <= numberToWrite; i++) {
+        console.log(i + " " + lineToWrite);
+    }
 }
 
 
 
 //task 4
-function task4(){
-const profName = prompt("Enter the name of a punishing professor");
-const lineToWrite = prompt("Enter a line for the user to write");
-const numberToWrite = parseInt(prompt("How many times should the user enter the line?"));
-const potterTag = document.querySelector(".potter");
-const profTag = document.querySelector(".professor");
+function task4() {
+    const profName = prompt("Enter the name of a punishing professor");
+    const lineToWrite = prompt("Enter a line for the user to write");
+    const numberToWrite = parseInt(prompt("How many times should the user enter the line?"));
+    const potterTag = document.querySelector(".potter");
+    const profTag = document.querySelector(".professor");
 
-let punishmentString = "";
-let i = 1;
-while(i <= numberToWrite){
-    punishmentString += `<li>${lineToWrite}</li>`;
-    i++;
-}
-profTag.innerHTML = profName;
-potterTag.innerHTML = punishmentString;
+    let punishmentString = "";
+    let i = 1;
+    while (i <= numberToWrite) {
+        punishmentString += `<li>${lineToWrite}</li>`;
+        i++;
+    }
+    profTag.innerHTML = profName;
+    potterTag.innerHTML = punishmentString;
 }
 
 
@@ -105,7 +105,7 @@ const task5 = () => {
 
     let punishmentString = "";
     let i = 1;
-    while(i <= numberToWrite){
+    while (i <= numberToWrite) {
         punishmentString += `<li>${lineToWrite}</li>`;
         i++;
     }
@@ -116,74 +116,78 @@ const task5 = () => {
 
 //task 6
 //part a
-function task6(){
-let firstFactor = 10;
-let secondFactor = 6;
+function task6() {
+    let firstFactor = 10;
+    let secondFactor = 6;
 
-for (let i = 1; i <= secondFactor; i++) {
-    let product = `${firstFactor} X ${i} = ${firstFactor * i}`;
-    console.log(product);
-}
-console.log("-------------------");
-
-
-//part b
-for (let i = 1; i <= 12; i++) {
-    createTimesTable(firstFactor, i);
-}
-
-//create a times table based on 2 factors
-function createTimesTable(firstFactor, secondFactor) {
-    let product = `${firstFactor} X ${secondFactor} = ${firstFactor * secondFactor}`;
-    console.log(product)
-}
-console.log("-------------------");
-
-
-//part c
-const allTables = (delimiter) => {
-    for (let factor = 1; factor <=12; factor++){
-    createFullTimesTable(factor, delimiter);
+    for (let i = 1; i <= secondFactor; i++) {
+        let product = `${firstFactor} X ${i} = ${firstFactor * i}`;
+        console.log(product);
     }
-};
+    console.log("-------------------");
 
-function createFullTimesTable(firstFactor, delimiter) {
-    for(let i = 1; i <= delimiter; i++){
-    let product = `${firstFactor} X ${i} = ${firstFactor * i}`;
-    console.log(product)
+
+    //part b
+    for (let i = 1; i <= 12; i++) {
+        createTimesTable(firstFactor, i);
     }
-}
 
-allTables(5);
+    //create a times table based on 2 factors
+    function createTimesTable(firstFactor, secondFactor) {
+        let product = `${firstFactor} X ${secondFactor} = ${firstFactor * secondFactor}`;
+        console.log(product)
+    }
+    console.log("-------------------");
+
+
+    //part c
+    const allTables = (delimiter) => {
+        for (let factor = 1; factor <= 12; factor++) {
+            createFullTimesTable(factor, delimiter);
+        }
+    };
+
+    function createFullTimesTable(firstFactor, delimiter) {
+        for (let i = 1; i <= delimiter; i++) {
+            let product = `${firstFactor} X ${i} = ${firstFactor * i}`;
+            console.log(product)
+        }
+    }
+
+    allTables(5);
 
 }
 
 
 //task 7
-function task7(){
-let z = 10;
-let v = 5;
+function task7() {
+    let z = 10;
+    let v = 5;
 
-//hoisting function, using the function before its declared; only works with function declaration
-foo(z, v); 
+    //hoisting function, using the function before its declared; only works with function declaration
+    foo(z, v);
 
-function foo(x, y) {
-    console.log(x + y);
-}
+    function foo(x, y) {
+        console.log(x + y);
+    }
 
-// I spent an hour figuring out why if you have a var called 'name',
-// it actually gets assigned and is not undefined as expected. Apparently browsers have a predefined var 'name'
-// property so it actually gets assigned, I love JS.
+    // I spent an hour figuring out why if you have a var called 'name',
+    // it actually gets assigned and is not undefined as expected. Apparently browsers have a predefined var 'name'
+    // property so it actually gets assigned, I love JS.
 
-//when you declare a variable it gets 'hoisted' to the top of the scope, this means that JS recognizes that the variable exists
-//no matter where you call that variable, even if you call it before its declared or initialized, however it will remain undefined until
-//the program gets to the line where its declared/initialized.
-//also only vars get fully hosted, consts and lets will go to some death zone until they get declared.
-console.log(name1);
+    //when you declare a variable it gets 'hoisted' to the top of the scope, this means that JS recognizes that the variable exists
+    //no matter where you call that variable, even if you call it before its declared or initialized, however it will remain undefined until
+    //the program gets to the line where its declared/initialized.
+    //also only vars get fully hosted, consts and lets will go to some death zone until they get declared.
 
-var name1 = "Luke";
+    console.log(name1); //will be 'undefined' because var name1 is hoisted, and assigned undefined until the line where the var is actually initialized
+    var name1 = "Luke";
 
-//The ECMA
+    /*
+    With many functions (function declaration, arrow functions, iife, so on) and different ways to 
+    declare variables (var, let, const), JS needed a way to ensure consistency, The ECMA was created 
+    for this purpose and defines what functions/ variables are hoisted and how they are hoisted.
+    */
 }
 
 //buttons to call tasks
